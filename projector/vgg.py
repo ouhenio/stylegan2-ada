24cc452dd4792c7bd9f905e50abcf466e5d06eeb
Eugenio Herrera <eiherrera@uc.cl>
2020-12-11 18:52:41 -0300

    Add new loss

--- projector/vgg.py
+++ projector/vgg.py
@@ -19,14 +19,14 @@ class PerceptualModel:
         self.loss = None
 
     def get_output_for(self, original_image, generated_image):
-        original_image = tf.transpose(original_image, (1, 2, 0))
-        generated_image = tf.transpose(generated_image, (1, 2, 0))
+        original_image = tf.transpose(original_image, (0, 2, 3, 1))
+        generated_image = tf.transpose(generated_image, (0, 2, 3, 1))
         vgg16 = tf.keras.applications.vgg16.VGG16(include_top=False, input_shape=(self.img_size, self.img_size, 3))
         self.perceptual_model = tf.keras.models.Model(vgg16.input, vgg16.layers[self.layer].output)
         # generated_image = preprocess_input(tf.image.resize_images(generated_image_tensor,
         #                                                           (self.img_size, self.img_size), method=1))
-        generated_image_features = self.perceptual_model([generated_image])
-        original_image_features = self.perceptual_model([original_image])
+        generated_image_features = self.perceptual_model(generated_image)
+        original_image_features = self.perceptual_model(original_image)
 
         # cosine_loss = tf.keras.losses.CosineSimilarity()
         # return -cosine_loss(original_image_features, generated_image_features)
